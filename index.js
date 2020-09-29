5cfc31bd8af7e15e95e44f2b6ffe9cf6a9d90f48
Sebastian Brear <ssbrear@ucdavis.edu>
2020-09-28 18:33:45 -0700

    Creating code skeleton

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!");
+  inquirer
+    .prompt({
+      type: "checkbox",
+      name: "action",
+      message: "How would you like to interact with the employee database?",
+      choices: [
+        "Add new entries",
+        "View existing entries",
+        "Update existing entries",
+      ],
+    })
+    .then((answer) => {
+        if (answer.action === "Add new entries") {
+            addEntries();
+        } else if (answer.action === 'View existing entries') {
+            viewEntries();
+        } else {
+            updateEntries();
+        }
+    });
+});
+
+function addEntries() {
+
+}
+
+function viewEntries() {
+
+}
+
+function updateEntries() {
+
+}
